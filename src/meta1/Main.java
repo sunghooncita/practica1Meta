2783919700e9817c3ac6b40e3211cb67c4a16c88
admin <carlopeich12@gmail.com>
2025-09-15 09:26:31 +0200

    hola2

--- src/meta1/Main.java
+++ src/meta1/Main.java
@@ -17,6 +17,18 @@ public class Main {
             System.out.println("\nMatriz de Distancias:");
             LeerMatriz.imprimirMatriz(distancias);
 
+            int[] solucion = Greedy.algoritmoGreedy(flujos, distancias);
+            int costo = Greedy.calcularCosto(solucion, flujos, distancias);
+
+            System.out.println("\n Solución encontrada:");
+            System.out.println(" Costo total: " + costo);
+
+            //Mostrar asignaciones
+            System.out.println("\n Asignación unidades a ubicaciones:");
+            for (int i = 0; i < solucion.length; i++) {
+                System.out.println("   Unidad " + i + ": Ubicación " + solucion[i]);
+            }
+
         } catch (FileNotFoundException e) {
             System.out.println("Error: " + e.getMessage());
         }
